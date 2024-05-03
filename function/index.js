718ac11ebe599888b9d58afb1976d79ca7436306
DenpaiID <rayhanalfarezy0@gmail.com>
2024-05-03 16:44:04 +0700

    V1

---
+++ function/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	blackboxAIChat: require('./scraper/ai'),
+	smartContract: require('./scraper/ai'),
+	ragBot: require('./scraper/ai'),
+	degreeGuru: require('./scraper/ai'),
+	igdl: require('./scraper/downloader'),
+	joox: require('./scraper/downloader'),
+	facebook: require('./scraper/downloader'),
+	tiktok: require('./scraper/downloader'),
+	pin: require('./scraper/downloader'),
+	twitter: require('./scraper/downloader')
+}
---
+++ function/scraper/ai.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+
+async function blackbox(message) {
+  try {
+    const response = await axios.post('https://www.blackbox.ai/api/chat', {
+      messages: [{ id: null, content: message, role: 'user' }],
+      id: null,
+      previewToken: null,
+      userId: null,
+      codeModelMode: true,
+      agentMode: {},
+      trendingAgentMode: {},
+      isMicMode: false,
+      isChromeExt: false,
+      githubToken: null
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+async function degreeGuru(message) {
+  try {
+    const response = await axios.post('https://degreeguru.vercel.app/api/guru', {
+      messages: [{ role: 'user', content: message }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+async function ragBot(message) {
+  try {
+    const response = await axios.post('https://ragbot-starter.vercel.app/api/chat', {
+      messages: [{ role: 'user', content: message }],
+      useRag: true,
+      llm: 'gpt-3.5-turbo',
+      similarityMetric: 'cosine'
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+async function smartContract(message) {
+  try {
+    const response = await axios.post("https://smart-contract-gpt.vercel.app/api/chat", {
+      messages: [{ content: message, role: "user" }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { 
+  blockbox,
+  degreeGuru,
+  ragBot,
+  smartContract
+}
